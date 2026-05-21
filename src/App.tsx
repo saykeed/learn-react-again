c7fde064163669fe20bd9258b61db0dc73bccd0f
saykeed-shuttlers <sherif.abubakri@shuttlers.ng>
2026-05-21 22:26:57 +0100

    project setup with tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-center p-3 text-4xl">
+        App Component
+      </div>
+    </>
+  )
+}
+
+export default App
